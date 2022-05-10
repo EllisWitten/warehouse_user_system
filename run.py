a49cadab1791e14c6950b51ad93d9b3b083797c0
EllisWitten <e.witten.2001@gmail.com>
2022-05-10 09:05:26 +0000

    add a function to check weather user name is correct

--- run.py
+++ run.py
@@ -11,3 +11,37 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('warehouse_user_system')
+
+user_login_worksheet = SHEET.worksheet('user_login')
+user_login_data = user_login_worksheet.get_all_values()
+
+def request_login_data():
+    """
+    Get user name and password
+    """
+    while True:
+        print('Welcome to the Warehouse User Software.')
+        print('PLease enter your username and password ...\n')
+        user_name = input('Username:')
+        password = input('Password:')
+        
+        if validate_user_name(user_name) == True:
+            print(f'Welcome {user_name}')
+            break
+        else:
+            print('Password or username was inccorect')
+    
+def validate_user_name(user_name):
+    all_user_names = []
+    for data in user_login_data[1:]:
+        all_user_names.append(data[0])
+    if user_name in all_user_names:
+        return True
+    else:
+        print('Username is incorrect')
+        return False
+    
+def main():
+    request_login_data()
+
+main()
